bb3a00092c835c114daf32a57d790ad559a3efd8
Brandon Dombrowsky <brandondombrowsky@gmail.com>
2022-01-10 12:43:42 -0800

    worked on random word button

--- button.js
+++ button.js
@@ -2,29 +2,26 @@ function changeColor() {
   var blueDiv = document.getElementById("changeable");
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
-  blueDiv.setAttribute('class', 'orange half')
+  blueDiv.setAttribute('class', 'orange half');
 }
 
-function replaceWord() {
+async function replaceWord() {
   var wordable = document.getElementById('wordable');
 
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+      "x-rapidapi-key": "295bc1e9b9msh5d9001a4dab9bacp1c707cjsn940bb4aab23b",
     },
   })
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word
+  });
+})
+.catch((err) => {
+  console.error(err);
+});
 }
