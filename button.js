8b341cbd24f304953327d30e6763e8216d46c53a
diegofalconc <diegofalconcostilla94@gmail.com>
2022-01-11 15:40:19 -0800

    week 1 assignment

--- button.js
+++ button.js
@@ -2,26 +2,24 @@ function changeColor() {
   var blueDiv = document.getElementById("changeable");
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
-  /* Your Code Goes Here */
+  blueDiv.setAttribute('class', 'purple half');
 }
 
 function replaceWord() {
   var wordable = document.getElementById('wordable');
 
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
+  fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', {
+    method: 'GET',
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      'x-rapidapi-host': config["x-rapidapi-host"],
+      'x-rapidapi-key': config["x-rapidapi-key"]
     },
   })
     .then((response) => {
       console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word;
       });
     })
     .catch((err) => {
