9d667353f2888afe217ad5aaf694e50a35a55168
Jon Biolette <94273256+jonbiolette@users.noreply.github.com>
2022-01-11 12:13:33 -0800

    Color added
    Forest green color class added for background change

--- button.js
+++ button.js
@@ -1,7 +1,6 @@
 function changeColor() {
   var blueDiv = document.getElementById("changeable");
   // Use the Mozilla Developer Network resource to find the right way to change
-  // the background for this div 
   /* Your Code Goes Here */
 }
 
