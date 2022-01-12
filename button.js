62d42f10bfac46952871c483779b18769f609267
Jon Biolette <94273256+jonbiolette@users.noreply.github.com>
2022-01-12 12:13:49 -0800

    Update word
    Updated replaceWord function to replace word upon event and display random word in console

--- button.js
+++ button.js
@@ -3,29 +3,23 @@ function changeColor() {
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
   /* Your Code Goes Here */
-    blueDiv.setAttribute('class', 'green half')
-
-
-
+    blueDiv.setAttribute('class', 'green half');
 }
 
-function replaceWord() {
+async function replaceWord() {
   var wordable = document.getElementById('wordable');
 
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
+  fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', {
+    method: 'GET',
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+      'x-rapidapi-key': 'keys'
     },
   })
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word
       });
     })
     .catch((err) => {
