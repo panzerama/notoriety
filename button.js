0d506489de17c8a81cf86647c438903c86857e62
Exochos <35015441+Exochos@users.noreply.github.com>
2022-01-11 20:44:46 -0800

    First commmit
    Added button and API calls as well as CSS

--- button.js
+++ button.js
@@ -1,30 +1,37 @@
 function changeColor() {
+  // Get the element to be changed
   var blueDiv = document.getElementById("changeable");
-  // Use the Mozilla Developer Network resource to find the right way to change
-  // the background for this div 
-  /* Your Code Goes Here */
-}
 
+  // Onclick, change the color of the element
+      blueDiv.setAttribute("class", "yellow half");
+}
+let key;
 function replaceWord() {
+  // Get the element to be changed
   var wordable = document.getElementById('wordable');
+
+  // Get the key to be used from the text file
+  fetch('key.txt')
+  .then(response => response.text())
+  .then(text => {
+    key = text;
+  })
+  .catch(error => console.log('error', error));
+  
 
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      "x-rapidapi-key": key,
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
     },
-  })
-    .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
-      });
     })
+    .then((response) => {
+      response.json().then((data) => {
+        wordable.innerHTML = data.word;
+      })
     .catch((err) => {
       console.error(err);
     });
+  });
 }
