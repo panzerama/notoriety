cad760fdad02bb06c3482a9f33e211edd5580127
Dipendra <dipshi8323@gmail.com>
2022-01-12 22:31:33 -0800

    changing word in div

--- button.js
+++ button.js
@@ -10,7 +10,7 @@ function replaceWord() {
     method: "GET",
     headers: {
       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "62ee04532cmsh7f31d7cd8adedf1p1e52cajsn58c49564488b'",
+      "x-rapidapi-key": "keys'",
     
     },
   })
