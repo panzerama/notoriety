c81ed4e3d2e96281391af8af95c6443006943589
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-01-13 15:31:26 -0800

    removeKey

--- button.js
+++ button.js
@@ -1,19 +1,23 @@
 function changeColor() {
   var blueDiv = document.getElementById("changeable");
-
-  blueDiv.setAttribute("class","green half")
-  
-  
-
+  blueDiv.setAttribute("class","green half") 
 }
+
 
 function replaceWord() {
   var wordable = document.getElementById('wordable');
 
+  fetch('key.txt')
+  .then(response => response.text())
+  .then(text => {
+    key = text;
+  }).catch((err) => {});
+
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "91b4866cf8mshaf4078eba316917p132b33jsn2011191e658c",
+      
+      "x-rapidapi-key": key,
       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
     },
   })
