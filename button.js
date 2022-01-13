735f5f5eaf218f4b4e17979ea2cd39f0da3590ea
arerde <arerde@uw.edu>
2022-01-12 20:55:39 -0800

    I made changes on button.js and main.css.

--- button.js
+++ button.js
@@ -3,25 +3,21 @@ function changeColor() {
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
   /* Your Code Goes Here */
+  blueDiv.setAttribute('class', 'orange half');
 }
 
-function replaceWord() {
+async function replaceWord() {
   var wordable = document.getElementById('wordable');
 
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
-    },
-  })
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
+       /* 
           Complete this handler function with code that populates an html 
           element with the random word 
         */
+      response.json().then((responsebody) => {
+        console.log(responseBody.word);
+      wordable.textContent = responseBody.word
       });
     })
     .catch((err) => {
