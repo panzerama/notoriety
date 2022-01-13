55ab7a4b3046a804a03689c0abc209c4bc23926a
kkennedy <kylekenne@gmail.com>
2022-01-13 15:06:34 -0800

    Adding all content to remote repo

--- button.js
+++ button.js
@@ -2,29 +2,26 @@ function changeColor() {
   var blueDiv = document.getElementById("changeable");
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
-  /* Your Code Goes Here */
+  blueDiv.setAttribute('class', 'green half')
 }
 
-function replaceWord() {
+async function replaceWord() {
   var wordable = document.getElementById('wordable');
 
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "827a06f261mshe8645cfdf526a17p107375jsncf83cf745b10",
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
     },
   })
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word
       });
     })
-    .catch((err) => {
+    .catch ((err) => {
       console.error(err);
     });
 }
