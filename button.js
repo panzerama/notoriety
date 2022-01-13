922d1dbe3bbf1e201af1e9db40127e14e8a29687
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-01-12 23:03:21 -0800

    The problem was subscription

--- button.js
+++ button.js
@@ -19,12 +19,12 @@ function replaceWord() {
   })
     .then((response) => {
       console.log(response);
-      response.json().then(() => {
+      response.json().then((responseBody) => {
         /* 
           Complete this handler function with code that populates an html 
           element with the random word 
         */
-
+          wordable.textContent = responseBody.word
 
       });
     })
