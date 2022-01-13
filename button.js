975d98c7f344f34fb9c08c376765fcecc62672d6
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-01-12 22:55:38 -0800

    fixed title for random word

--- button.js
+++ button.js
@@ -13,13 +13,13 @@ function replaceWord() {
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "",
-      "x-rapidapi-host": "",
+      "x-rapidapi-key": "91b4866cf8mshaf4078eba316917p132b33jsn2011191e658c",
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
     },
   })
     .then((response) => {
       console.log(response);
-      response.json().then(() => {git 
+      response.json().then(() => {
         /* 
           Complete this handler function with code that populates an html 
           element with the random word 
