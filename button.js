28dbcd31d8ad4e5181ad52048a41a2d4815356f0
kkennedy <kylekenne@gmail.com>
2022-01-17 20:39:22 -0800

    new commit

--- button.js
+++ button.js
@@ -1,27 +1,10 @@
 function changeColor() {
   var blueDiv = document.getElementById("changeable");
-  // Use the Mozilla Developer Network resource to find the right way to change
-  // the background for this div 
   blueDiv.setAttribute('class', 'green half')
 }
-
-async function replaceWord() {
+function replaceWord() {
   var wordable = document.getElementById('wordable');
-
-  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "827a06f261mshe8645cfdf526a17p107375jsncf83cf745b10",
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-    },
-  })
-    .then((response) => {
-      response.json().then((responseBody) => {
-        console.log(responseBody.word);
-        wordable.textContent = responseBody.word
-      });
-    })
-    .catch ((err) => {
-      console.error(err);
-    });
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) => wordable.textContent = json.userId.toString());
 }
