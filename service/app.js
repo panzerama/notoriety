7353d298edc1ae4004ece03313f7241d8a36c38b
diegofalconc <diegofalconcostilla94@gmail.com>
2022-01-22 21:52:04 -0800

    Card functionality and express route

--- service/app.js
+++ service/app.js
@@ -1,19 +1,21 @@
 import express from 'express'
+import cors from 'cors'
+import cardData from './data/cards.json'
+
 
 const app = express()
-const port = 8000
+const port = 8002
 
-/**
- * We need this middleware to send and receive JSON data easily. We'll cover
- * middleware later
- */
 app.use(express.json())
+app.use(cors)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Assignment: Add a new route here for GET /cards
+app.get('/cards', (req, res) => {
+  res.send(cardData)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
