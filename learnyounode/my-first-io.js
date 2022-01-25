10490e994545a686c3e9290a69429daca255410d
arerde <arerde@uw.edu>
2022-01-24 18:28:41 -0800

    exer3 from learnyounode

---
+++ learnyounode/my-first-io.js
@@ -0,0 +1,7 @@
+const fs = require('fs'); // imported library
+let file = process.argv[2];
+
+let x = fs.readFileSync(file, "utf8"); //printing the text file
+console.log(x.split("\n").length-1);
+
+//console.log(x.toString());  -- print out the the file
